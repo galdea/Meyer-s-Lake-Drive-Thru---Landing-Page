cd81b55dc5b502d3e7bcf90f5d14f51173eb2256
Gabriel Aldea D <gabriel.aldea@gmail.com>
2023-05-24 14:14:20 -0400

    contact form

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,165 +1,146 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-class Contact extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      email: '',
-      message: '',
-    };
+export const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function encode(data) {
+    return Object.keys(data)
+      .map(
+        (key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]),
+      )
+      .join('&');
   }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { name, email, message } = this.state;
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
     fetch('/', {
       method: 'POST',
       headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: new URLSearchParams({
-        'form-name': 'contact',
-        name: name,
-        email: email,
-        message: message,
-      }).toString(),
+      body: encode({ 'form-name': 'contact', name, email, message }),
     })
       .then(() => {
-        console.log('Form successfully submitted');
-        this.setState({
-          name: '',
-          email: '',
-          message: '',
-        });
+        alert('Message sent, I will be contacting you soon!');
+        window.location.reload();
       })
       .catch((error) => alert(error));
   };
 
-  handleChange = (event) => {
-    this.setState({
-      [event.target.id]: event.target.value,
-    });
-  };
-
-  render() {
-    const { name, email, message } = this.state;
-    const { address, phone, email: contactEmail } = this.props.data || {};
-    return (
-      <div>
-        <div id="contact">
-          <div className="container">
-            <div className="col-md-8">
-              <div className="row">
-                <div className="section-title">
-                  <h2>Get In Touch</h2>
-                  <p>
-                    Please fill out the form below to send us an email and we
-                    will get back to you as soon as possible.
-                  </p>
+  return (
+    <div>
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="section-title">
+                <h2>Get In Touch</h2>
+                <p>
+                  Please fill out the form below to send us an email and we will
+                  get back to you as soon as possible.
+                </p>
+              </div>
+              <form
+                name="contact"
+                method="POST"
+                action="/"
+                onSubmit={handleSubmit}
+                id="contactForm"
+                noValidate
+                data-netlify="true"
+              >
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="form-control"
+                        placeholder="Name"
+                        required="required"
+                        onChange={(e) => setName(e.target.value)}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Email"
+                        required="required"
+                        onChange={(e) => setEmail(e.target.value)}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
                 </div>
-                <form
-                  name="contact"
-                  method="POST"
-                  onSubmit={this.handleSubmit}
-                  id="contactForm"
-                  noValidate
-                  data-netlify="true"
-                  data-netlify-honeypot="bot-field"
-                >
-                  <input type="hidden" name="form-name" value="contact" />
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input
-                          type="text"
-                          id="name"
-                          className="form-control"
-                          placeholder="Name"
-                          required="required"
-                          value={name}
-                          onChange={this.handleChange}
-                        />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input
-                          type="email"
-                          id="email"
-                          className="form-control"
-                          placeholder="Email"
-                          required="required"
-                          value={email}
-                          onChange={this.handleChange}
-                        />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <textarea
-                      name="message"
-                      id="message"
-                      className="form-control"
-                      rows="4"
-                      placeholder="Message"
-                      required
-                      value={message}
-                      onChange={this.handleChange}
-                    ></textarea>
-                    <p className="help-block text-danger"></p>
-                  </div>
-                  <div id="success"></div>
-                  <button type="submit" className="btn btn-custom btn-lg">
-                    Send Message
-                  </button>
-                </form>
-              </div>
-            </div>
-
-            <div className="col-md-3 col-md-offset-1 contact-info">
-              <div className="contact-item">
-                <h3>Contact Info</h3>
-                <p>
-                  <span>
-                    <i className="fa fa-map-marker"></i> Address
-                  </span>
-                  {address || 'loading'}
-                </p>
-              </div>
-              <div className="contact-item">
-                <p>
-                  <span>
-                    <i className="fa fa-phone"></i> Phone
-                  </span>{' '}
-                  {phone || 'loading'}
-                </p>
-              </div>
-              <div className="contact-item">
-                <p>
-                  <span>
-                    <i className="fa fa-envelope-o"></i> Email
-                  </span>{' '}
-                  {contactEmail || 'loading'}
-                </p>
-              </div>
+                <div className="form-group">
+                  <textarea
+                    type="text"
+                    id="message"
+                    name="message"
+                    className="form-control"
+                    rows="4"
+                    placeholder="Message"
+                    required="required"
+                    onChange={(e) => setMessage(e.target.value)}
+                  ></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div id="success"></div>
+                <button type="submit" className="btn btn-custom btn-lg">
+                  Send Message
+                </button>
+              </form>
             </div>
           </div>
-        </div>
-        <div id="footer">
-          <div className="container text-center">
-            <p>
-              &copy; 2022 Designed by{' '}
-              <a href="http://www.gabrielaldea.com" rel="nofollow">
-                Gabriel Aldea
-              </a>
-            </p>
+
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className="fa fa-map-marker"></i> Address: 4301 12th St Nw,
+                  Canton, OH 44708
+                </span>
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-phone"></i> Phone: +1 330-477-7008
+                </span>
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email:
+                  meyerslakedrivethru@gmail.com
+                </span>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+      <div id="footer">
+        <div className="container text-center">
+          <p>
+            &copy; 2022 Designed by
+            <a href="http://www.gabrielaldea.com" rel="nofollow">
+              Gabriel Aldea
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
